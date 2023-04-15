aaf3f48fce6de55efad4a8bce937ee946a2c0f3d
manish3203 <manishchavan3203@gmail.com>
2023-04-15 22:34:06 +0530

    If-Else Statements In Java

---
+++ Statements/if-else/Class_Codes/1_Program.java
@@ -0,0 +1,14 @@
+//Given an integer age as input
+//print "Eligible to vote" if the person is eligible to vote 
+
+class Voting {
+
+	public static void main(String[] args){
+
+		int age = 20;
+
+		if(age > 18){
+			System.out.println("Eligible for vote");
+		}
+	}
+}
---
+++ Statements/if-else/Class_Codes/6_Program.java
@@ -0,0 +1,27 @@
+// Given an integer value as input
+// -print fizz if it is divisible by 3
+// -print buzz if it is divisible by 5
+// -print fizz-buzz if it is divisible by both
+// -print Not Divisible by both if it is divisible by 3 and 5
+
+class Divisible{
+
+        public static void main(String[] agrs){
+
+                int num = 17;
+
+                if((num % 3 == 0) && (num % 5 == 0)){
+                        System.out.println("Fizz-Buzz");
+                
+		}else if(num % 3 == 0){
+                        System.out.println("Fizz");
+                
+		}else if(num % 5 == 0){
+                        System.out.println("Buzz");
+		
+		}else{
+                        System.out.println("Not divisible by both");
+		}
+
+        }
+}
