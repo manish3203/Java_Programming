493c154aee327570c4d1dddeccae349903000209
manish3203 <manishchavan3203@gmail.com>
2023-04-24 22:33:11 +0530

    Number Systems Codes Using Methods

---
+++ Methods/Practice_Codes/1_Program.java
@@ -0,0 +1,38 @@
+/*
+  Program 1 : Prime number in range 
+*/
+
+import java.io.*;
+
+class Primenum{
+
+	static void prime_Num(int start,int end){
+		
+		System.out.println("Prime Number Range Between " + start + " to " + end + " : ");
+		for(int i = start; i <= end; i++){
+			int count = 0;
+			for(int j = 1; j <= i;j++){
+				
+				if(i % j == 0){
+					count++;
+				}
+			}
+			if(count == 2){
+				
+				System.out.println(i);
+			}
+		}
+	}
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter the starting number : ");
+		int start = Integer.parseInt(br.readLine());
+
+		System.out.println("Enter the ending number : ");
+		int end = Integer.parseInt(br.readLine());
+
+		prime_Num(start,end);
+	}
+}
---
+++ Methods/Practice_Codes/3_Program.java
@@ -0,0 +1,44 @@
+/*
+  Program 3 : Stronf NUmber in range
+*/
+
+import java.io.*;
+
+class StrongNum{
+	
+	static void Strong_Number(int start,int end){
+		
+		for(int i = start; i <= end; i++){
+			
+			int temp = i;
+			int sum = 0;
+			while(temp != 0){
+				
+				int rem = temp % 10;
+				int fact = 1;
+				for(int j = 1 ; j <= rem; j++){
+					
+					fact = fact * j;
+				}
+				sum = sum + fact;
+				temp = temp / 10;
+			}
+			if(i == sum){
+				System.out.println(i);
+			}
+
+		}
+	}
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter the starting number : ");
+		int start = Integer.parseInt(br.readLine());
+		
+		System.out.println("Enter the ending number : ");
+		int end = Integer.parseInt(br.readLine());
+
+		Strong_Number(start,end);
+	}
+}
