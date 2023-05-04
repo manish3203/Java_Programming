5ae3187740276401e2fa995601349e6febe0736c
manish3203 <manishchavan3203@gmail.com>
2023-05-04 22:45:39 +0530

    Length Method In String

---
+++ String/Methods/2_StrLength.java
@@ -0,0 +1,19 @@
+/*
+  Methods : public int length();
+
+  Descripton : It returns the number of characters contained is given string.
+
+  Parameter : No Parameter
+
+  Return Type : Integer
+
+*/
+
+class LengthDemo {
+
+	public static void main(String[] args){
+
+		String str1 = "Core2Web";
+		System.out.println(str1.length());
+	}
+}
