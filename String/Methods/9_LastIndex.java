09274f1ab04787aa93bc63344a1269f2385dc05b
manish3203 <manishchavan3203@gmail.com>
2023-05-08 19:58:33 +0530

    LastIndexOf Methods In String

---
+++ String/Methods/9_LastIndex.java
@@ -0,0 +1,19 @@
+/*
+  Method : public int lastIndexOf(int ch, int fromIndex);
+
+  Description : Find the last instance of character in the given string
+
+  Parameter : character (ch to find),Integer(index of starts the search)
+
+  Return Type : Integer
+*/
+
+class LastIndex {
+
+	public static void main(String[] args){
+
+		String str1 = "shashi";
+
+		System.out.println(str1.lastIndexOf('h',1));
+	}
+}
