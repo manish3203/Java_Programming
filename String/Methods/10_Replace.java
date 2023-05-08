6e0c3f697b5ebae977068328880c8bd1798bfb4a
manish3203 <manishchavan3203@gmail.com>
2023-05-08 19:59:09 +0530

    Replace Methods In String

---
+++ String/Methods/10_Replace.java
@@ -0,0 +1,20 @@
+/*
+  Method : public string replace(char oldChar, Char newChar);
+
+  Description : replaces every instance of character in the given string with a new character
+
+  Paramter : character (old char), character (new char)
+
+  Return Type : String
+
+*/
+
+class ReplaceString {
+
+	public static void main(String[] args) {
+
+		String str1 = "MJNISH";
+
+		System.out.println(str1.replace('J','A'));
+	}
+}
