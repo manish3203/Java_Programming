27fbf40704d0b5f65f599738c306701fe0eb6b97
manish3203 <manishchavan3203@gmail.com>
2023-05-08 19:58:04 +0530

    IndexOf Methods In String

---
+++ String/Methods/8_IndexOf.java
@@ -0,0 +1,20 @@
+/*
+  Methods : public int indexOf(char ch, int form index);
+
+  Description : Find the first instance of the character in the given string
+
+  Paramter : character (ch to find), Integer(index to start the search)
+
+  Return Type : Integer
+
+*/
+
+class IndexOf {
+
+	public static void main(String[] args) {
+
+		String str1 = "Manish";
+
+		System.out.println(str1.indexOf('i',0));
+	}
+}
