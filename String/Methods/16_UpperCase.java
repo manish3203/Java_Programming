812c596a47d3659fc2b588fca653d01dcfc81f13
manish3203 <manishchavan3203@gmail.com>
2023-05-23 21:51:22 +0530

    String toUpperCase Method In Java

---
+++ String/Methods/16_UpperCase.java
@@ -0,0 +1,20 @@
+/*
+  Method : public String toUpperCase();
+
+  Description : it uppercases to this string.
+
+  Paramter : No Paramter
+
+  return type : String
+
+*/
+
+class ToUpperCases {
+
+	public static void main(String[] args) {
+
+		String str = "Core2Web";
+
+		System.out.println(str.toUpperCase());
+	}
+}
