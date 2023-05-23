73fb8f454111590c9819c3e5214c4e1549572a43
manish3203 <manishchavan3203@gmail.com>
2023-05-23 21:49:23 +0530

    Substring Method (start to end) In Java

---
+++ String/Methods/14_SubString.java
@@ -0,0 +1,21 @@
+/*
+  Method : public String substring(int i, int j);
+
+  Description : creates the substring of given String starting at specified
+                index and ending at one character before the specified index.
+
+  Parameters : Integer(Starting index), Integer(Ending index)
+
+  return type : String
+
+*/
+
+class SubString {
+
+	public static void main(String[] args) {
+
+		String str = "Core2Web";
+
+		System.out.println(str.substring(5,8));
+	}
+}
