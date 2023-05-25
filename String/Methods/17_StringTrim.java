78f658e77ca5a8578d62642264b1eb93948130d1
manish3203 <manishchavan3203@gmail.com>
2023-05-25 22:33:56 +0530

    String trim Method In Java

---
+++ String/Methods/17_StringTrim.java
@@ -0,0 +1,21 @@
+/*
+   Method : public String trim();
+
+   Description : trims all the white spaces before and after the String.
+                 ex : str.trim()
+
+   Parameters : no parameter
+
+   return type : String
+
+*/
+
+class Trim {
+
+	public static void main(String[] args) {
+
+		String str = "Know The Code Till The Core ";
+
+		System.out.println(str.trim());
+	}
+}
