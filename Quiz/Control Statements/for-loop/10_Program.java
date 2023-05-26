33f9177fec7c12d5c69a838f340ba0a9d13ce59b
manish3203 <manishchavan3203@gmail.com>
2023-05-26 12:17:34 +0530

    ForLoop Basic Quiz In Java

---
+++ Quiz/Control Statements/for-loop/10_Program.java
@@ -0,0 +1,9 @@
+class Core2Web {
+	public static void main(String[] args) {
+
+		int var1 = 5;
+
+		for(int i = 1; i<=var1;i++)
+			int x = var1/i;
+	}
+}
---
+++ Quiz/Control Statements/for-loop/5_Program.java
@@ -0,0 +1,10 @@
+class Core2Web {
+	public static void main(String[] args) {
+		int var1 = 5;
+		
+                for(int i=1;i<=5;i++)
+			System.out.println("i = "+i);
+		
+                System.out.println("i = "+i);
+	}
+}
