b08bd156681186dcd2980a99066f3ff2b45387ff
manish3203 <manishchavan3203@gmail.com>
2023-06-03 14:06:31 +0530

    Instance Var Concept In Java

---
+++ Inheritance/Class_Codes/7_Program.java
@@ -0,0 +1,34 @@
+
+class Parent {
+
+	int x = 10;
+	
+	Parent() {
+
+		System.out.println("In Parent Constructor");
+	}
+	void access() {
+
+		System.out.println("Parent Instance");
+	}
+}
+class Child extends Parent {
+
+	int y =20;
+
+	Child() {
+
+		System.out.println("In Child Constructor");
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
+class Client {
+
+	public static void main(String[] args) {
+
+		Child obj = new Child();
+		obj.access();
+	}
+}
+		
