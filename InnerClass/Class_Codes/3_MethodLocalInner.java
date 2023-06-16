f30198b91c0fd11604f6785a8e822b0ef10458dc
manish3203 <manishchavan3203@gmail.com>
2023-06-16 09:44:50 +0530

    Method Local Inner Class Code In Java

---
+++ InnerClass/Class_Codes/3_MethodLocalInner.java
@@ -0,0 +1,37 @@
+//Method Local Inner Class
+
+class Outer {
+
+	void m1() {
+
+		System.out.println("In-m1 Outer");
+
+		class Inner {
+
+			void m1() {
+
+				System.out.println("In-m1 Inner");
+			}
+		}
+		Inner obj = new Inner();
+		obj.m1();
+	}
+	void m2() {
+
+		System.out.println("In-m2 Outer");
+	}
+	public static void main(String[] args) {
+
+		Outer obj = new Outer();
+		obj.m1();
+		obj.m2();
+	}
+}
+
+/*
+   There are 2 .class unique file 
+  'Outer$1Inner.class'   
+   Outer.class
+*/
+
+
