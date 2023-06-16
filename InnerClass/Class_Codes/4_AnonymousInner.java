2ae2052094b3771b5e4ee553dcc2ad360842aec7
manish3203 <manishchavan3203@gmail.com>
2023-06-16 09:45:52 +0530

    Anonymous Inner Class Code In Java

---
+++ InnerClass/Class_Codes/4_AnonymousInner.java
@@ -0,0 +1,24 @@
+
+//Anonymous class
+
+class Demo {
+
+}
+class Client {
+
+	public static void main(String[] args) {
+
+		Demo obj = new Demo() {
+
+		};
+	}
+}
+
+/*
+ In this code there are 3 .class file
+ 'Client$1.class'   
+  Client.class   
+  Demo.class
+
+*/
+
