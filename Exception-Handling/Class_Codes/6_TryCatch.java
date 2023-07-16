2684c341af5a6ead1b63914547f252892bb13edf
manish3203 <manishchavan3203@gmail.com>
2023-07-16 21:32:36 +0530

    Try Catch Cocnept In Java

---
+++ Exception-Handling/Class_Codes/6_TryCatch.java
@@ -0,0 +1,17 @@
+
+class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start Main");
+
+		try {
+			System.out.println(10/0);
+		} catch(ArithmeticException obj) {
+
+			System.out.println("Exception Occured");
+		}
+
+		System.out.println("End Main");
+	}
+}
