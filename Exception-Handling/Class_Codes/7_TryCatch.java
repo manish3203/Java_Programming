08051973018a357dee5293a983c3cc806607c912
manish3203 <manishchavan3203@gmail.com>
2023-07-16 21:38:09 +0530

    Try Catch Cocnept In Java

---
+++ Exception-Handling/Class_Codes/7_TryCatch.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+class ExceptionDemo {
+
+	public static void main(String[] args)throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		System.out.println(str);
+
+		int data = 0;
+
+		try {
+			data = Integer.parseInt(br.readLine());
+		} catch(NumberFormatException obj) {
+
+			System.out.println("Please Enter Integer Data");
+			data = Integer.parseInt(br.readLine());
+		}
+		System.out.println(data);
+	}
+}
+
