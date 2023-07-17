f7cf159ac540c0c5df12cf7bbb0f39e72ac96e58
manish3203 <manishchavan3203@gmail.com>
2023-07-17 21:53:22 +0530

    Array 01 Assignment/ Interview Preparation

--- Interview Preparation/Array/Array01/9_Program.java
+++ Interview Preparation/Array/Array01/9_Program.java
@@ -20,6 +20,48 @@
 import java.io.*;
 
 class StrongNumber {
+	
+	void strongNum(int arr[]) {
+		
+		for(int i=0; i<arr.length; i++) {
 
+			int temp = arr[i];               //teporary variable 
+			int sum = 0;
 
+			while(temp != 0) {
+				
+				int fact = 1;
+				int rem = temp%10;
+
+				for(int j=1; j<=rem; j++){
+					
+					fact = fact*j;
+				}
+				sum = sum+fact;
+				temp= temp/10;
+			}
+			if(sum == arr[i]){
+				
+				System.out.println("The Strong Number In The Array Is : "+sum);
+			}
+		}
+	}
+	public static void main(String[] args)throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter Size Of An Array");
+		int n = Integer.parseInt(br.readLine());
+
+		int arr[] = new int[n];
+		
+		System.out.println("Enter elements in the array : ");
+		for(int i=0; i<arr.length; i++) {
+			
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		StrongNumber obj = new StrongNumber();
+		obj.strongNum(arr);
+	}
 }
