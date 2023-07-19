e74d097f331ce598296b68f431ad8de518b9fabe
manish3203 <manishchavan3203@gmail.com>
2023-07-19 21:24:26 +0530

    StackOverFlow Exception In Java

---
+++ Exception-Handling/Class_Codes/10_StackOver.java
@@ -0,0 +1,15 @@
+
+class StackDemo {
+	
+	static void fun(int x) {
+
+		fun(++x);
+	}
+	public static void main(String[] args) {
+		
+		fun(1);
+	}
+}
+
+// Exception in thread "main" java.lang.StackOverflowError
+
