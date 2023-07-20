ab77ee00843061f2320cb87428418798d0f49653
manish3203 <manishchavan3203@gmail.com>
2023-07-20 20:52:06 +0530

    start can't be override concept

---
+++ MultiThreading/Class_Codes/3_start.java
@@ -0,0 +1,25 @@
+//Start Can't Be Override In Java
+
+class MyThread extends Thread {
+
+	public void run() {
+
+		System.out.println("In run");
+		System.out.println(Thread.currentThread().getName());
+	}
+	public void start() {
+
+		System.out.println("In my thread start");
+		run();
+	}
+}
+class ThreadDemo {
+
+	public static void main(String[] args) {
+
+		MyThread obj = new MyThread();
+		obj.start();
+
+		System.out.println(Thread.currentThread().getName());
+	}
+}
