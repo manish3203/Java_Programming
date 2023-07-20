5e25fbd86a51f7abd4030467bd3c4c693a5008f0
manish3203 <manishchavan3203@gmail.com>
2023-07-20 21:03:52 +0530

    Runnable Interface In MultiThreading concept

---
+++ MultiThreading/Class_Codes/5_Runnable.java
@@ -0,0 +1,27 @@
+
+class MyThread implements Runnable {
+
+	public void run() {
+
+		System.out.println("In run");
+		System.out.println(Thread.currentThread().getName());
+	}
+}
+class ThreadDemo {
+
+	public static void main(String[] args) {
+
+		MyThread obj = new MyThread();
+
+		Thread t = new Thread(obj);
+		t.start();
+
+		System.out.println(Thread.currentThread().getName());
+	}
+}
+
+/*
+main
+In run
+Thread-0
+*/
