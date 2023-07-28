1b1b7edd9ff7927fc80407f63d31f54e5f183fd3
manish3203 <manishchavan3203@gmail.com>
2023-07-28 22:07:09 +0530

    CachedThreadPool Concept

---
+++ MultiThreading/Class_Codes/20_ThreadPool.java
@@ -0,0 +1,43 @@
+
+import java.util.concurrent.*;
+
+class MyThread implements Runnable {
+
+	int num;
+	MyThread(int num) {
+
+		this.num = num;
+	}
+	public void run() {
+
+		System.out.println(Thread.currentThread() + " Start " + num);
+		
+		dailyTask();
+		
+		System.out.println(Thread.currentThread() + " End " + num);
+	}
+	void dailyTask() {
+
+		try {
+
+			Thread.sleep(7000);
+		}catch(InterruptedException ie) {
+
+		}
+	}
+}
+
+class ThreadPoolDemo {
+
+	public static void main(String[] args) {
+
+		ExecutorService ser = Executors.newCachedThreadPool();
+
+		for(int i=1; i<=6; i++) {
+
+			MyThread obj = new MyThread(i);
+			ser.execute(obj);
+		}
+		ser.shutdown();
+	}
+}
