f1841fe9301c8c67707dd617b330d1cf6503549d
manish3203 <manishchavan3203@gmail.com>
2023-08-08 16:13:02 +0530

    Collection : LinkedList Concept & Methods

---
+++ Collection/Class_Codes/3_LinkedList.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class LinkedListDemo {
+
+	public static void main(String[] args) {
+
+		LinkedList ll = new LinkedList();
+
+		ll.add(20);
+		ll.addFirst(10);
+		ll.addLast(30);
+
+		System.out.println(ll);
+	}
+}
