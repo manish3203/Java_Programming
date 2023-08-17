c85a3eba1db8e2bdffdf53bbd0c53804d76e8ae5
manish3203 <manishchavan3203@gmail.com>
2023-08-17 23:29:45 +0530

    Collection : LinkedHashMap Concept

---
+++ Collection/Class_Codes/27_LinkedHashMap.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+class LinkedHashMapDemo {
+
+	public static void main(String[] args) {
+
+		LinkedHashMap lhm = new LinkedHashMap();
+
+		lhm.put("Kanha","Infosys");
+		lhm.put("Ashish","barclays");
+		lhm.put("Badhe","BMC");
+		lhm.put("Rahul","IBM");
+
+		System.out.println(lhm);
+	}
+}
