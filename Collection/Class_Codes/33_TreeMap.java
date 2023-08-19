317b08fb503be5bf24d3094c251132758043a53b
manish3203 <manishchavan3203@gmail.com>
2023-08-19 11:46:49 +0530

    Collection :- TreeMap Concept

---
+++ Collection/Class_Codes/33_TreeMap.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class TreeMapDemo {
+
+	public static void main(String[] args) {
+
+		TreeMap tm = new TreeMap();
+		
+		tm.put("Ind","India");
+		tm.put("Pak","Pakistan");
+		tm.put("SL","SriLanka");
+		tm.put("Ban","Bangladesh");
+		tm.put("Aus","Australia");
+		
+		System.out.println(tm);
+	}
+}
+
+//{Aus=Australia, Ban=Bangladesh, Ind=India, Pak=Pakistan, SL=SriLanka}
+
