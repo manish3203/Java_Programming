0be7897bd2d5ce2340970127f3237506445f5245
manish3203 <manishchavan3203@gmail.com>
2023-08-19 18:08:05 +0530

    HashTable Concept

---
+++ Collection/Class_Codes/38_HashTable.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+class HashTableDemo {
+
+	public static void main(String[] args) {
+
+		Hashtable ht = new Hashtable();
+
+		ht.put(10,"Sachine");
+		ht.put(7,"Infosys");
+		ht.put(18,"virat");
+		ht.put(45,"Rohit");
+		ht.put(1,"Rahul");
+
+		System.out.println(ht);
+	}
+}
+
+/*
+ 
+{10=Sachine, 18=virat, 7=Infosys, 1=Rahul, 45=Rohit}
+
+*/
