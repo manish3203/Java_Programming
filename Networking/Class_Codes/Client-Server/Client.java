51ed029b89d4e523be6a1e911cc6211ea669ec23
manish3203 <manishchavan3203@gmail.com>
2023-09-13 15:55:57 +0530

    Client Side Code

---
+++ Networking/Class_Codes/Client-Server/Client.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.net.*;
+
+class Client {
+
+	public static void main(String[] args)throws IOException {
+
+		Socket s = new Socket("localhost",1200);
+		InputStream is = s.getInputStream();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		String str;
+
+		while((str = br.readLine()) != null) {
+
+			System.out.println(str);
+		}
+		br.close();
+		s.close();
+	}
+}
